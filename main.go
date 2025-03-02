728f34ec84e5c8098279b6927b5727faeb7f2cf4
zhengran <zhengran@kuaishou.com>
2025-03-02 13:05:51 +0000

    add log module

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"github.com/Csy12139/Vesper/cmd"
+	"github.com/Csy12139/Vesper/log"
 	"github.com/urfave/cli/v2"
-	"log"
 	"os"
 )
 
@@ -17,6 +17,11 @@ func run(args []string) error {
 }
 
 func main() {
+	if err := log.InitLog("./", 1024, 10, "info"); err != nil {
+		panic(err)
+	}
+	log.Debug("this is debug log")
+	log.Info("this is info log")
 	if err := run(os.Args); err != nil {
 		log.Fatal(err)
 	}
