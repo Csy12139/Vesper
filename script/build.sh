93303be9dbd550405655bfd9d17f9e8293f09af1
cissy <1787393382@qq.com>
2025-03-29 11:22:28 +0800

    fix script

--- script/build.sh
+++ script/build.sh
@@ -8,19 +8,16 @@ mkdir -p build
 
 # Generate protobuf code
 echo "Generating protobuf code..."
-#protoc --go_out=. --go_opt=paths=source_relative \
-#    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
-#    ../proto/*.proto
-protoc --proto_path=../proto --go_out=../proto --go_opt=paths=source_relative \
-    --go-grpc_out=../proto --go-grpc_opt=paths=source_relative \
-    ../proto/mn_service.proto
+protoc --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    ./proto/*.proto
 
 # Build DN binary
 echo "Building DN..."
-go build -o build/dn ../DN
+go build -o build/dn ./DN
 
 # Build MN binary 
 echo "Building MN..."
-go build -o build/mn ../MN
+go build -o build/mn ./MN
 
 echo "Build complete! Binaries are in the build directory"
